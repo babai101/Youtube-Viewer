75fd06072675d0eef10dd5274f17ee9dc73513b8
Soumyadeep Chanda <deep.fatality@gmail.com>
2017-11-01 17:07:35 +0000

    added play overlay button and buffering animation

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('youtubeApp', ['ngResource', 'ngRoute', 'jtt_youtube', 'ngSanitize', 'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls']);
+var app = angular.module('youtubeApp', ['ngResource', 'ngRoute', 'jtt_youtube', 'ngSanitize', 'com.2fdevs.videogular', 'com.2fdevs.videogular.plugins.controls', 'com.2fdevs.videogular.plugins.overlayplay', 'com.2fdevs.videogular.plugins.buffering']);
 
 app.config(function($routeProvider, $httpProvider) {
     $routeProvider
